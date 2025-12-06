0b1a45509779efabbeb9ede3577af3231d5b13c5
Bakulya <blarsenova@gmail.com>
2025-12-05 20:05:09 -0600

    Lists and data types

--- GH Python Classes/TypesInPY.py
+++ GH Python Classes/TypesInPY.py
@@ -9,3 +9,31 @@ print(myfloat)
 a, b, c = 1,2,4
 print(a,b,c)
 
+remainder =10%2.4
+print(remainder)
+
+multiplying= 7*2
+squared =7**2
+cubed = 2**3
+print(multiplying, squared, cubed)
+
+lotsofhellos = "hello"*9
+print(lotsofhellos)
+
+####### Lists######
+
+myList =[1,2,3]
+myList.append(6)
+print(len(myList))
+
+myListNames = ['Moe', 'Zoe', 'Zee']
+myListNames.insert(1, 'Bee')
+print(myListNames)
+
+myListNames.extend(['New Name Extended here'])
+print(myListNames)
+myListNames.index('Moe')
+print(myListNames.index('Zoe'))
+myListNames.sort()
+print(myListNames)
+
