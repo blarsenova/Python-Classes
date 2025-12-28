5f994cc1081d3b69a7e72b4ad3f0b28835da51db
Bakulya <blarsenova@gmail.com>
2025-12-27 18:30:27 -0600

    sorted array in python for strngs

--- GH Python Classes/SortedArray.py
+++ GH Python Classes/SortedArray.py
@@ -2,3 +2,9 @@
 my_list = [3, 1, 4, 1, 5, 9, 2]
 sorted_list = sorted(my_list)
 print(sorted_list)
+
+
+# For strings
+chars = ['d', 'a', 'c', 'boy', 'girl']
+sorted_chars = sorted(chars)
+print(sorted_chars)
