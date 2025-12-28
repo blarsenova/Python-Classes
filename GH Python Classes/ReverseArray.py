bc7a1289bb0a453648e131d293c678e76690288e
Bakulya <blarsenova@gmail.com>
2025-12-28 17:27:04 -0600

    reversing an array w slicing method

--- GH Python Classes/ReverseArray.py
+++ GH Python Classes/ReverseArray.py
@@ -1,4 +1,14 @@
-
+#1
 arr = [1, 2, 3, 4, 5]
 reversed_arr = arr[::-1]
 print(reversed_arr)
+
+#2 w reversed w list
+arr = [11, 4, 3, 68, 5]
+reversed_arr = list(reversed(arr))
+print(reversed_arr)
+
+#3 method (in-place)
+arr = [42,32,66]
+arr.reverse()
+print(arr)
