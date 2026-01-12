96f7a2178ee4d7f607969c695540001e372fb1fa
Bakulya <blarsenova@gmail.com>
2026-01-11 21:00:42 -0600

    week 2, string functions

--- GH Python Classes/StringManipulations.py
+++ GH Python Classes/StringManipulations.py
@@ -7,4 +7,10 @@ print(astring.count("s"))
 #slice or splice
           #012345678
 astring1= "Very long Sentence"
-print(astring1 [3:7])
+print(astring1 [4::1])#start, end, steps
+print(astring1[::-1])
+
+#UpperCase-LowerCase
+astring2="Elephant"
+print(astring2.upper())
+print(astring2.startswith("Elephan"))
