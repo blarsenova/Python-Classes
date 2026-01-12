0032514cc850fd1b9c49cc71fe932d10865bdde4
Bakulya <blarsenova@gmail.com>
2026-01-11 20:40:52 -0600

    week 2, string functions

--- GH Python Classes/StringManipulations.py
+++ GH Python Classes/StringManipulations.py
@@ -2,3 +2,9 @@
 astring = 'words and sentencces'
 print(astring.index("r"))
 print(astring.count("s"))
+
+
+#slice or splice
+          #012345678
+astring1= "Very long Sentence"
+print(astring1 [3:7])
