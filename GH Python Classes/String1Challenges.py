92614fbd5e3d474ddadbd36eef959c23cf246fb7
Bakulya <blarsenova@gmail.com>
2026-01-12 21:35:31 -0600

    solution for C

--- GH Python Classes/String1Challenges.py
+++ GH Python Classes/String1Challenges.py
@@ -9,3 +9,20 @@ def donuts(count):
 
 print(donuts(3))
 print(donuts(19))
+
+
+#C
+def fix_start(s):
+
+    front = s[0]
+
+    back = s[1:]
+
+    # Replace the front character inside the back string
+    replaced = back.replace(front, '*')
+
+    # Combine them back together
+    return front + replaced
+
+print(fix_start("babble"))
+print(fix_start("vveryyy"))
