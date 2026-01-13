b9b4864e3c5975e9d5c7b034f3789e3fa792e3a7
Bakulya <blarsenova@gmail.com>
2026-01-12 21:15:38 -0600

    added a solution for D task

---
+++ GH Python Classes/String2 challenges.py
@@ -0,0 +1,16 @@
+
+#D - verbing
+def verbing(s):
+    # Check length first
+    if len(s) < 3:
+        return s
+
+    # If length is 3 or more, check the ending
+    if len(s) >= 3 and s.endswith('ing'):
+        s += 'ly'
+    else:
+        s += 'ing'
+
+    return s
+
+
