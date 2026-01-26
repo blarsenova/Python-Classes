8b957840129a620b327b6bc6afea2b86f3a1c555
Bakulya <blarsenova@gmail.com>
2026-01-25 20:58:22 -0600

    Week-7 - HW's - C solution for tuples

--- GH Python Classes/List1py_HW's.py
+++ GH Python Classes/List1py_HW's.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 
 #B
 
@@ -12,5 +13,11 @@ for words in allWords:
     else:plainList.append(words)
 
 
-print(sorted(listX) + sorted(plainList))
+print(sorted(listX) + sorted(plainList))   #print(sorted(grade, key=itemgetter(0,-1)))
+
+#C - tuples, sorted by increasing last number
+tuple1=[(1,7), (5,3), (1,4,4), (2,2), (2,8), (1,1)]
+print(sorted(tuple1, key=itemgetter(-1)))
+
+
 
