8b63bb88d4ccd534eccabf5ef0338c0dc76bd9a3
Bakulya <blarsenova@gmail.com>
2026-02-05 18:54:49 -0600

    week 8 - readandwrite

---
+++ GH Python Classes/ReadWrite.py
@@ -0,0 +1,24 @@
+
+# Open the file you just created
+f = open('/Users/baktygullarsenova/HelloWorld/Test python file 1', 'rt', encoding='utf-8')
+
+
+for line in f:
+    print(line, end='')
+
+f.close()
+
+path = '/Users/baktygullarsenova/HelloWorld/Test python file 1.txt'
+
+# Step 1: Write to the file
+f2 = open(path, 'w', encoding='utf-8')
+f2.write('This is custom msg string')
+f2.close() # You MUST close it to save the changes before reading
+
+# Step 2: Read from the file
+f2 = open(path, 'rt', encoding='utf-8')
+for line in f2:
+    print(line, end='')
+f2.close()
+
+
